7c372242040e4f1185b0db314a594ac9ae397fb0
Farook0 <farooqayoub01@gmail.com>
2024-08-21 22:49:27 +0500

    Data Entry

---
+++ vision.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from PIL import Image
+load_dotenv()
+
+import streamlit as st
+import os
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model=genai.GenerativeModel("gemini-1.5-flash")
+def get_gemini_response(input,image):
+    if input !="":
+        response=model.generate_content([input,image])
+    else:
+        response=model.generate_content(image)    
+    return response.text
+
+#intilze our streamlit page
+st.set_page_config(page_title="Gemini App to extract text from image")
+st.header("Gemini Application")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file=st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png","wpeg"])
+image=""
+if uploaded_file is not None:
+    image=Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+submit=st.button("Tell me about the image")
+#if submit button is clicked
+if submit:
+    response=get_gemini_response(input, image)
+    st.subheader("The response is")
+    st.write(response)
